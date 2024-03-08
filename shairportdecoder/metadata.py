a03334261f1418eb8ce934293bc2c261345f8bfb
tristangough <tristangough@gmail.com>
2024-03-08 13:45:02 -0500

    Update metadata.py

--- shairportdecoder/metadata.py
+++ shairportdecoder/metadata.py
@@ -126,10 +126,10 @@ class Infos(object):
 		return (self.appleclient if self.appleclient else "Apple device")
 
 	def to_connect(self):
-		return (self.connect if self.connect else "")
+		return True
 
 	def to_disconnect(self):
-		return (self.disconnect if self.disconnect else "")
+		return False
 
 class CoverArt(object):
 	def __init__(self, base64=None, binary=None, mime=None, extension=None, checksum=None):
