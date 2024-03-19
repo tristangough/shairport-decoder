ad860d6c483d2440590694a7ecbad0b133a61472
tristangough <tristangough@gmail.com>
2024-03-19 15:24:03 -0400

    Update metadata.py

--- shairportdecoder/metadata.py
+++ shairportdecoder/metadata.py
@@ -144,7 +144,7 @@ class Infos(object):
 		return (self.disconnect)
 
 	def to_test(self):
-		return (self.phbt) + (self.styp) + (self.abeg)
+		return (self.abeg)
 
 class CoverArt(object):
 	def __init__(self, base64=None, binary=None, mime=None, extension=None, checksum=None):
