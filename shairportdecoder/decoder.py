6be723a51bde90d705b4ee03d97e915abaa81bdf
tristangough <tristangough@gmail.com>
2024-03-20 00:42:06 -0400

    Update decoder.py

--- shairportdecoder/decoder.py
+++ shairportdecoder/decoder.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from luckydonaldUtils.encoding import unicode_type
 from luckydonaldUtils.logger import logging
-from shairportdecoder.metadata import Infos, Item, CoverArt
+from shairportdecoder.metadata import Infos, Item, CoverArt, WriteName
 
 __author__ = 'luckydonald'
 
